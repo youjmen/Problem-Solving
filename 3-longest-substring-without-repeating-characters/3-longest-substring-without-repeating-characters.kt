2f8a3c9c526d217ca9106e722f09318bddda33d2
Jaemin Yu <48317457+youjmen@users.noreply.github.com>
2022-03-24 01:46:52 +0900

    Time: 588 ms (21.23%), Space: 36.1 MB (94.14%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.kt
@@ -0,0 +1,29 @@
+class Solution {
+    // 로직 구상을 하다가 중간에 풀리지 않아 솔루션을 봄. 슬라이딩 윈도우를 통해 푸는 문제
+// 브루트 포스로 작성 시 O(N^3)이 걸린다.
+    fun lengthOfLongestSubstring(s: String): Int {
+    var left = 0
+    var right = 0
+    var result = 0
+    val subStringLengthList = IntArray(128){0}
+    while (right < s.length){
+        val char = s[right]
+        subStringLengthList[char.toInt()]++
+        while (subStringLengthList[char.toInt()]>1){
+            val l = s[left]
+            subStringLengthList[l.toInt()]--
+            left++
+        }
+        val size = right - left + 1
+        println("size $size")
+        println("left $left")
+        println("right $right")
+
+        result = Math.max(result, size)
+        println("result $result")
+        right++
+    }
+    return result
+
+    }
+}
